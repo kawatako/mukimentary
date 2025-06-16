a2e602bcadd9d22a78d04ede3f9fe6db2a4f78e1
kawatashota <qhqn_hqnd@icloud.com>
2025-06-16 23:19:31 +0900

    PWA設定

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,21 +1,32 @@
+// next.config.js
+import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-images: {
-  remotePatterns: [
-    {
-      protocol: "http",
-      hostname: "localhost",
-      port: "4566",
-      pathname: "/uploads/**",
-    },
-    {
-      protocol: "https",
-      hostname: "uploads-mukimentary.s3.ap-northeast-3.amazonaws.com",
-      pathname: "/uploads/**",
-    },
-  ],
-}
+const baseConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4566",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "uploads-mukimentary.s3.ap-northeast-3.amazonaws.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
+//PWAの設定
+export default withPWA({
+  ...baseConfig,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
 
-export default nextConfig;
